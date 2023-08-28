02e3b3b4e9ce20b04d6b6d07c4e78dde7a60d98f
ideamostuweb <info@ideamostuweb.com>
2023-08-28 14:52:50 -0500

    Se agregan y organizan mejor los metodos

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -31,8 +31,6 @@ class Model
 
     /**
      * Constructor de la clase Model.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -40,66 +38,74 @@ class Model
     }
 
     /**
-     * Obtiene todos los registros de una tabla.
+     * Crea un nuevo registro en la base de datos.
      *
      * @param string $table Nombre de la tabla.
-     * @param array $columns Columnas a seleccionar (opcional).
-     * @param array $where Condiciones de consulta (opcional).
-     * @return array Resultados de la consulta.
+     * @param array $data Datos del registro a crear.
+     * @return bool|int Id del registro creado o false en caso de error.
      */
-    public function getAll($table, $columns = ['*'], $where = [])
-    {
-        return $this->db->select($table, $columns, $where);
-    }
-
-    /**
-     * Obtiene un solo registro de una tabla por su ID.
-     *
-     * @param string $table Nombre de la tabla.
-     * @param int $id ID del registro a obtener.
-     * @return array|null Resultado de la consulta o null si no se encuentra.
-     */
-    public function getById($table, $id)
-    {
-        return $this->db->get($table, '*', ['id' => $id]);
-    }
-
-    /**
-     * Inserta un nuevo registro en la tabla.
-     *
-     * @param string $table Nombre de la tabla.
-     * @param array $data Datos del registro a insertar.
-     * @return bool True si la inserción se realiza con éxito, de lo contrario, false.
-     */
-    public function insert($table, $data)
+    public function create($table, $data)
     {
         return $this->db->insert($table, $data);
     }
 
     /**
-     * Actualiza un registro existente en la tabla por su ID.
+     * Obtiene un registro por su ID.
      *
      * @param string $table Nombre de la tabla.
-     * @param int $id ID del registro a actualizar.
-     * @param array $data Nuevos datos del registro.
-     * @return bool True si la actualización se realiza con éxito, de lo contrario, false.
+     * @param int $id ID del registro a buscar.
+     * @return array|bool Registro encontrado o false si no se encuentra.
      */
-    public function updateById($table, $id, $data)
+    public function findById($table, $id)
+    {
+        return $this->db->get($table, '*', ['id' => $id]);
+    }
+
+    /**
+     * Actualiza un registro en la base de datos.
+     *
+     * @param string $table Nombre de la tabla.
+     * @param array $data Datos a actualizar.
+     * @param int $id ID del registro a actualizar.
+     * @return bool Resultado de la operación.
+     */
+    public function update($table, $data, $id)
     {
         return $this->db->update($table, $data, ['id' => $id]);
     }
 
     /**
-     * Elimina un registro de la tabla por su ID.
+     * Elimina un registro de la base de datos.
      *
      * @param string $table Nombre de la tabla.
      * @param int $id ID del registro a eliminar.
-     * @return bool True si la eliminación se realiza con éxito, de lo contrario, false.
+     * @return bool Resultado de la operación.
      */
-    public function deleteById($table, $id)
+    public function delete($table, $id)
     {
         return $this->db->delete($table, ['id' => $id]);
     }
 
-    // Otros métodos comunes relacionados con modelos...
+    /**
+     * Obtiene todos los registros de una tabla.
+     *
+     * @param string $table Nombre de la tabla.
+     * @return array Registros encontrados.
+     */
+    public function findAll($table)
+    {
+        return $this->db->select($table, '*');
+    }
+
+    /**
+     * Busca registros en una tabla según condiciones dadas.
+     *
+     * @param string $table Nombre de la tabla.
+     * @param array $conditions Condiciones de búsqueda.
+     * @return array Registros encontrados.
+     */
+    public function findByCondition($table, $conditions)
+    {
+        return $this->db->select($table, '*', $conditions);
+    }
 }
